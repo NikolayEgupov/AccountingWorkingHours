20312f22e86cb48ca7f5b44f92d6773dbe5ab578
NikolayEgupov <Rhfcyjdrf250601>
2023-02-23 15:47:39 +0700

    Initial commit

---
+++ src/main/java/ru/egupov/accountingworkinghours/AccountingWorkingHoursApplication.java
@@ -0,0 +1,13 @@
+package ru.egupov.accountingworkinghours;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingWorkingHoursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingWorkingHoursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/egupov/accountingworkinghours/AccountingWorkingHoursApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.egupov.accountingworkinghours;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingWorkingHoursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
